4589bdaa1007c240484fc12b74aa9c6448c9b68c
kamran <askerari@mail.ru>
2023-02-16 19:33:03 +0300

    Создали массив

---
+++ itog/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size  = int.Parse(Console.ReadLine());
+Console.Clear();
+string[] array1 = new string[size];
+
+
+
+
+for (int i = 0; i < size; i++){
+	Console.Write("Введите данные: ");
+    string result = Console.ReadLine();
+	array1[i] = result; 
+Console.Clear(); 
+}
+
+
+
+
+Console.WriteLine();
+Console.Write("Массив: [");
+Console.Write(string.Join(",", array1));
+Console.Write("]");
+Console.Clear();
+
+int count = 0;
+int m = 3;
+
+for (int i = 0; i < array1.Length; i++){
+	if(array1[i].Length <= m){
+		count++;
+	}   
+}
